ae0183a708327e3441176d8bde29820d098ac3d1
yxliaoyx <yxliaoyx@gmail.com>
2025-03-18 20:02:05 +0800

    fix: auto shutdown scripts

--- Example Server/Abiotic Factor/auto_shutdown.sh
+++ Example Server/Abiotic Factor/auto_shutdown.sh
@@ -1,14 +1,12 @@
-established_ssh=$(ss --no-header state established '( dport = :ssh or sport = :ssh )')
+tcpdump_output=$(timeout 600 tcpdump -c 1 port 7777 2>/dev/null)
 
-if [[ -n $established_ssh ]]; then
+if [[ -n $tcpdump_output ]]; then
     exit 0
 fi
 
-# 123 (NTP)
-# 27015 (SRCDS)
-tcpdump_output=$(timeout 600 tcpdump -c 1 udp and not port 123 and not port 27015 2>/dev/null)
+established_ssh=$(ss --no-header state established '( dport = :ssh or sport = :ssh )')
 
-if [[ -z $tcpdump_output ]]; then
+if [[ -z $established_ssh ]]; then
     systemctl stop abioticfactor.service
     shutdown now
 fi
--- Example Server/Core Keeper/auto_shutdown.sh
+++ Example Server/Core Keeper/auto_shutdown.sh
@@ -1,13 +1,12 @@
-established_ssh=$(ss --no-header state established '( dport = :ssh or sport = :ssh )')
+tcpdump_output=$(timeout 600 tcpdump -c 1 port 27015 2>/dev/null)
 
-if [[ -n $established_ssh ]]; then
+if [[ -n $tcpdump_output ]]; then
     exit 0
 fi
 
+established_ssh=$(ss --no-header state established '( dport = :ssh or sport = :ssh )')
 
-tcpdump_output=$(timeout 600 tcpdump -c 10 port 27015 2>/dev/null)
-
-if [[ -z $tcpdump_output ]]; then
+if [[ -z $established_ssh ]]; then
     systemctl stop corekeeper.service
     shutdown now
 fi
